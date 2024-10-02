02e09d0b52a32784a703e96e4be9dc1cfb1bd996
ehitimum <ehitimumchoudhury@gmail.com>
2024-10-03 01:21:48 +0600

    Add:1.0.11.3

---
+++ iot/src/main/java/com/yunyan/project/iot/dto/bindDevice/bindDTO.java
@@ -0,0 +1,15 @@
+package com.yunyan.project.iot.dto.bindDevice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class bindDTO {
+    private String uid;
+}
