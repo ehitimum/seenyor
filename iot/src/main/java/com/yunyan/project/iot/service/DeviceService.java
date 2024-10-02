150a77cff4cab4503086e03721d67d753844d911
ehitimum <ehitimumchoudhury@gmail.com>
2024-10-03 01:22:17 +0600

    Add:1.0.11.7

--- iot/src/main/java/com/yunyan/project/iot/service/DeviceService.java
+++ iot/src/main/java/com/yunyan/project/iot/service/DeviceService.java
@@ -1,6 +1,8 @@
 package com.yunyan.project.iot.service;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
@@ -9,14 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.yunyan.project.iot.dto.DeviceResponse;
-import com.yunyan.project.iot.dto.MqttEnableDTO;
-import com.yunyan.project.iot.dto.properties.DeviceDTO;
+import com.yunyan.project.iot.dto.bindDevice.bindDTO;
+import com.yunyan.project.iot.dto.mqttDisable.MqttDisableDTO;
+import com.yunyan.project.iot.dto.mqttDisable.MqttDisableResponse;
+import com.yunyan.project.iot.dto.mqttEnable.MqttEnableDTO;
 import com.yunyan.project.iot.dto.properties.PropertiesDTO;
 import com.yunyan.project.iot.util.Sha1Util;
 
@@ -55,46 +60,69 @@ public class DeviceService {
     public DeviceResponse enableMqttPush(MqttEnableDTO request) throws NoSuchAlgorithmException {
         String apiUrl = "https://qinglanst.com/prod-api/thirdmqtt/v2/dept/opmqtt";
         String timestamp = String.valueOf(System.currentTimeMillis() / 1000);
+        Map<String, Object> body = new HashMap<>();
+        
+        // Set request body parameters
+        body.put("uid", request.getUid());
+        body.put("server", request.getServer());
+        body.put("port", request.getPort());
+        body.put("username", request.getUsername());
+        body.put("password", request.getPassword());
+        body.put("clientId", request.getClientId());
+        body.put("keepAlive", request.getKeepAlive());
+        body.put("messageType", request.getMessageType());
+        
+        if (request.getTopics() != null && request.getTopics().getPub() != null) {
+            Map<String, List<String>> topics = new HashMap<>();
+            topics.put("pub", request.getTopics().getPub());
+            body.put("topics", topics);
+        }
+
+        // Concatenate and create signature (same as before)
         Map<String, String> params = new TreeMap<>();
-    
         params.put("uid", request.getUid());
         params.put("server", request.getServer());
         params.put("port", String.valueOf(request.getPort()));
         params.put("username", request.getUsername());
         params.put("password", request.getPassword());
         params.put("clientId", request.getClientId());
-        params.put("keepAlive", String.valueOf(request.getKeepAlive())); 
+        params.put("keepAlive", String.valueOf(request.getKeepAlive()));
         params.put("messageType", String.join("=", request.getMessageType().stream()
-                                       .map(String::valueOf)
-                                       .toArray(String[]::new)));
+                                        .map(String::valueOf)
+                                        .toArray(String[]::new)));
         if (request.getTopics() != null && request.getTopics().getPub() != null) {
             params.put("topics", "pub=" + String.join("=", request.getTopics().getPub()));
         }
-    
+
         String concatenatedParams = params.entrySet().stream()
             .map(entry -> entry.getKey() + "=" + entry.getValue())
             .collect(Collectors.joining("#"));
-    
+
         String concatenatedString = secret + "#" + timestamp + "#" + concatenatedParams + "#";
         System.out.println(concatenatedString);
         String signature = Sha1Util.generateSha1(concatenatedString);
         System.out.println(signature);
+
+        // Create headers and add Content-Type
         HttpHeaders headers = new HttpHeaders();
         headers.add("appid", appId);
         headers.add("timestamp", timestamp);
         headers.add("signature", signature);
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl);
-        HttpEntity<?> entity = new HttpEntity<>(headers);
+        headers.setContentType(MediaType.APPLICATION_JSON);  // Set Content-Type to application/json
+
+        // Create HttpEntity with body and headers
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        // Make POST request
         ResponseEntity<DeviceResponse> response = restTemplate.exchange(
-                builder.toUriString(),
+                apiUrl,
                 HttpMethod.POST,
                 entity,
                 DeviceResponse.class
         );
-        return response.getBody();
         
-    
-    }
+        return response.getBody();
+}
 
     public PropertiesDTO getDeviceProp() throws NoSuchAlgorithmException {
         String apiUrl = "https://qinglanst.com/prod-api/thirdparty/v2/deviceProp?uid=25A859B81A6F";
@@ -126,4 +154,119 @@ public class DeviceService {
         return response.getBody();
     }
 
+    public MqttDisableResponse disableMqtt(MqttDisableDTO request) throws NoSuchAlgorithmException {
+        
+        String apiUrl = "https://qinglanst.com/prod-api/thirdmqtt/v2/dept/clmqtt";
+        String timestamp = String.valueOf(System.currentTimeMillis()/ 1000);
+        Map<String, Object> body = new HashMap<>();
+        body.put("uid", request.getUid());
+        body.put("messageType", request.getMessageType());
+        Map<String, String> params = new TreeMap<>();
+        params.put("uid", request.getUid());
+        params.put("messageType", String.join("=", request.getMessageType().stream()
+                                        .map(String::valueOf)
+                                        .toArray(String[]::new)));
+        
+        String concatenatedParams = params.entrySet().stream()
+                                        .map(entry -> entry.getKey() + "=" + entry.getValue())
+                                        .collect(Collectors.joining("#"));
+
+        String concatenatedString = secret + "#" + timestamp + "#" + concatenatedParams + "#";
+        System.out.println(concatenatedString);
+        String signature = Sha1Util.generateSha1(concatenatedString);
+        System.out.println(signature);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("appid", appId);
+        headers.add("timestamp", timestamp);
+        headers.add("signature", signature);
+        headers.setContentType(MediaType.APPLICATION_JSON);  
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<MqttDisableResponse> response = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.POST,
+                entity,
+                MqttDisableResponse.class
+        );
+        
+        return response.getBody();
+    }
+
+    public DeviceResponse bindDevice(bindDTO request) throws NoSuchAlgorithmException {
+        String apiUrl = "https://qinglanst.com/prod-api/thirdparty/v2/bindDevice";
+        String timestamp = String.valueOf(System.currentTimeMillis()/ 1000);
+        Map<String, Object> body = new HashMap<>();
+        body.put("uid", request.getUid());
+        Map<String, String> params = new TreeMap<>();
+        params.put("uid", request.getUid());
+        
+        String concatenatedParams = params.entrySet().stream()
+                                        .map(entry -> entry.getKey() + "=" + entry.getValue())
+                                        .collect(Collectors.joining("#"));
+
+        String concatenatedString = secret + "#" + timestamp + "#" + concatenatedParams + "#";
+        System.out.println(concatenatedString);
+        String signature = Sha1Util.generateSha1(concatenatedString);
+        System.out.println(signature);
+
+       
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("appid", appId);
+        headers.add("timestamp", timestamp);
+        headers.add("signature", signature);
+        headers.setContentType(MediaType.APPLICATION_JSON); 
+
+      
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+
+      
+        ResponseEntity<DeviceResponse> response = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.POST,
+                entity,
+                DeviceResponse.class
+        );
+        
+        return response.getBody();
+    }
+
+    public DeviceResponse unbindDevice(bindDTO request) throws NoSuchAlgorithmException {
+        String apiUrl = "https://qinglanst.com/prod-api/thirdparty/v2/unbindDevice";
+        String timestamp = String.valueOf(System.currentTimeMillis()/ 1000);
+        Map<String, Object> body = new HashMap<>();
+        body.put("uid", request.getUid());
+        Map<String, String> params = new TreeMap<>();
+        params.put("uid", request.getUid());
+        
+        String concatenatedParams = params.entrySet().stream()
+                                        .map(entry -> entry.getKey() + "=" + entry.getValue())
+                                        .collect(Collectors.joining("#"));
+
+        String concatenatedString = secret + "#" + timestamp + "#" + concatenatedParams + "#";
+        System.out.println(concatenatedString);
+        String signature = Sha1Util.generateSha1(concatenatedString);
+        System.out.println(signature);
+
+       
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("appid", appId);
+        headers.add("timestamp", timestamp);
+        headers.add("signature", signature);
+        headers.setContentType(MediaType.APPLICATION_JSON); 
+
+      
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+
+      
+        ResponseEntity<DeviceResponse> response = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.POST,
+                entity,
+                DeviceResponse.class
+        );
+        
+        return response.getBody();
+    }
 }
