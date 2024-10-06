cfa3402584a0701d4569eded8a27ce3b4f3d1c51
ehitimum <ehitimumchoudhury@gmail.com>
2024-10-07 00:27:11 +0600

    Add:1.0.13.3

---
+++ iot/src/main/java/com/yunyan/project/iot/dto/boundary/DeviceAreaDTO.java
@@ -0,0 +1,20 @@
+package com.yunyan.project.iot.dto.boundary;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeviceAreaDTO {
+    private String uid;
+    private String coordKey;
+    private Byte type;
+    private String upLeft;
+    private String lowLeft;
+    private String upRight;
+    private String lowRight; 
+}
