be1550676e9192c642a8dd88b7cf3aa0c65d5571
ehitimum <ehitimumchoudhury@gmail.com>
2024-10-08 01:18:16 +0600

    Add:1.0.14.10

---
+++ iot/src/main/java/com/yunyan/project/iot/model/Whitelist.java
@@ -0,0 +1,17 @@
+package com.yunyan.project.iot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Whitelist {
+    private String whiteNumber;
+    private String whiteNumberName;
+    private String status;
+}
