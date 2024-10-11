d5cffcf7ae7071a2d5e55ca6d1b417c1a1dcf6cb
ehitimum <ehitimumchoudhury@gmail.com>
2024-10-11 18:42:54 +0600

    Add:1.0.16.1

---
+++ iot/src/main/java/com/yunyan/project/iot/model/BreathRate.java
@@ -0,0 +1,20 @@
+package com.yunyan.project.iot.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BreathRate {
+    private int min;
+    private int avg;
+    private int max;
+    private String avgLabel;
+    private List<DataPoint> dataList;
+}
